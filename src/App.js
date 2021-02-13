a93ab4b046d802de44fdc002a3eaf68f75007f19
ChironJon <immrwhiskers4@gmail.com>
2021-02-12 18:41:21 -0600

    updating app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { createStructuredSelector } from 'reselect';
 
 import './App.css';
@@ -11,38 +11,32 @@ import CatalogPage from './pages/CatalogPage/catalogpage.component';
 import LoginPage from './pages/LoginPage/loginpage.component';
 import Footer from './components/footer/footer.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    };
-  }
+
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
+          setCurrentUser({
                 id: snapShot.id,
                 ...snapShot.data()
-              }
-            }
-          );
+          });
         });
-      } else {
-        this.setState({currentUser: userAuth})
       }
+      
+      setCurrentUser(userAuth)
     });
   }
+  
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -52,12 +46,12 @@ class App extends React.Component {
     return (
       <div className='App'>
         {/*header*/}
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         {/*pages*/}
         <Switch>
           <Route exact path='/acnh-isle-comp' component={StartPage} />
           <Route path='/acnh-isle-comp/catalog' component={CatalogPage} />
-          <Route path='/acnh-isle-comp/login' component={LoginPage} /> 
+          <Route exact path='/acnh-isle-comp/login' render={() => this.props.currentUser ? (<Redirect to='/acnh-isle-comp' />) : (<LoginPage />)} /> 
         </Switch>
         {/*footer*/}
         <Footer />
@@ -66,4 +60,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 
 import directoryReducer from './directory/directory.reducers';
 import collectionReducer from './collection/collection.reducers';
-
+import userReducer from './user/user.reducers';
 
 export default combineReducers({
   directory: directoryReducer,
-  collections: collectionReducer
+  collections: collectionReducer,
+  user: userReducer
 })
