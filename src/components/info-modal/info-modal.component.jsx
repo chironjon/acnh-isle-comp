151700385aac67102f64f8a5be4d818b2922071d
ChironJon <immrwhiskers4@gmail.com>
2021-02-20 23:53:38 -0600

    info modal hide-able. adding info card functionality next

--- src/components/info-modal/info-modal.component.jsx
+++ src/components/info-modal/info-modal.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 import ItemCard from '../item-card/item-card.component';
 
 import './info-modal.styles.scss';
 
-const InfoModal = ({item}) => (
-  <div className='info-modal-container'>
-    <ItemCard item={item}/>
-    
-  </div>
+const InfoModal = ({item, hidden}) => (
+  <>
+    {
+      hidden ? null :
+      <div className='info-modal-container'>
+        <ItemCard item={item}/>
+      </div>
+    }
+  </>
 )
 
-export default InfoModal;
+const mapStateToProps = ({ info: { hidden }}) => ({
+  hidden
+})
+
+export default connect(mapStateToProps)(InfoModal);
